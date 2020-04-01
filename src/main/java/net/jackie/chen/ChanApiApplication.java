7c89cd472aee7a136e448b25da7a49599fd37df1
chenjiaqi <jiaqi.chen@sky-cloud.net>
2020-04-01 12:50:41 +0800

    feat: commit

---
+++ src/main/java/net/jackie/chen/ChanApiApplication.java
@@ -0,0 +1,13 @@
+package net.jackie.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class ChanApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChanApiApplication.class, args);
+    }
+}
